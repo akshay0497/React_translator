1531132b71016b827a5d31fbc4d5e9fc29e4e167
Akshay Kumar Gupta <1997akshay04@gmail.com>
2024-07-15 12:57:46 +0530

    translator add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+// language list for example only
+import Languages from './languages'
+
+// import component
+import { ReactTransliterate } from "react-transliterate";
+//import "react-transliterate/dist/index.css";
+
+// Material Ui input component
+import Input from "@material-ui/core/Input";
+
+const App = () => {
+  const [text, setText] = useState("");
+
+  const [lang, setLang] = useState("en");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Language Translator</h2>
+
+      <select
+        className="language-dropdown"
+        value={lang}
+        onChange={(e) => setLang(e.target.value )}
+      >
+        {Languages.map((l) => (
+          <option key={l.value} value={l.value}>
+            {l.label}
+          </option>
+        ))}
+      </select>
+
+      <div className="spacer" />
+
+      <label htmlFor="react-transliterate-input">Using input</label>
+      <ReactTransliterate
+        value={text}
+        onChangeText={(text) => {
+          setText(text);
+        }}
+        lang={lang}
+        placeholder="Start typing here..."
+        id="react-transliterate-input"
+      />
+
+      <div className="spacer" />
+
+      <label htmlFor="react-transliterate-textarea">Using textarea</label>
+      <ReactTransliterate
+        renderComponent={(props) => <textarea {...props} />}
+        value={text}
+        onChangeText={(text) => {
+          setText(text);
+        }}
+        lang={lang}
+        placeholder="Start typing here..."
+        id="react-transliterate-textarea"
+      />
+
+      <div className="spacer" />
+
+      <label htmlFor="react-transliterate-material-ui-input">
+        Using Material UI input
+      </label>
+      <ReactTransliterate
+        renderComponent={(props) => {
+          const inputRef = props.ref;
+
+          delete props["ref"];
+
+          return <Input fullWidth {...props} inputRef={inputRef} />;
+        }}
+        value={text}
+        onChangeText={(text) => {
+          setText(text);
+        }}
+        lang={lang}
+        placeholder="Start typing here..."
+        id="react-transliterate-material-ui-input"
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import './index.css'
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+
+// ReactDOM.render(<App />, document.getElementById('root'))
